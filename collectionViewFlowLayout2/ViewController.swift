3878ed5fb56cffc27e5d8b3ee7097bc9f55468ed
Yulia Milovanova <preachermars@gmail.com>
2020-12-07 23:27:27 +0300

    Initial Commit

---
+++ collectionViewFlowLayout2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionViewFlowLayout2
+//
+//  Created by Preacher on 07.12.2020.
+//  Copyright © 2020 Yulia Milovanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
