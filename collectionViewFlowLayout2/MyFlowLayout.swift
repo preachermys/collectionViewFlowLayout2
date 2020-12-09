c78c16bc82c51ebf1ede9e18621dffa909108bc1
Yulia Milovanova <preachermars@gmail.com>
2020-12-09 14:26:47 +0300

    Сделала пример

---
+++ collectionViewFlowLayout2/MyFlowLayout.swift
@@ -0,0 +1,45 @@
+//
+//  MyFlowLayout.swift
+//  collectionViewFlowLayout2
+//
+//  Created by Preacher on 08.12.2020.
+//  Copyright © 2020 Yulia Milovanova. All rights reserved.
+//
+
+
+import UIKit
+
+class MyFlowLayout : UICollectionViewFlowLayout {
+    // how to left-justify every "line" of the layout
+    // looks much nicer, in my humble opinion
+    
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let arr = super.layoutAttributesForElements(in: rect)!
+        return arr.map { atts in
+            var atts = atts
+            if atts.representedElementCategory == .cell {
+                let ip = atts.indexPath
+                atts = self.layoutAttributesForItem(at:ip)!
+            }
+            return atts
+        }
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        var atts = super.layoutAttributesForItem(at:indexPath)!
+        if indexPath.item == 0 {
+            return atts // degenerate case 1
+        }
+        if atts.frame.origin.x - 1 <= self.sectionInset.left {
+            return atts // degenerate case 2
+        }
+        let ipPv = IndexPath(item:indexPath.row-1, section:indexPath.section)
+        let fPv = self.layoutAttributesForItem(at:ipPv)!.frame
+        let rightPv = fPv.origin.x + fPv.size.width + self.minimumInteritemSpacing
+        atts = atts.copy() as! UICollectionViewLayoutAttributes
+        atts.frame.origin.x = rightPv
+        return atts
+    }
+
+}
